2a82b538502e6d828f500c1ae945d1b9fc4c4661
John <jrdjacoby@gmail.com>
2024-03-12 16:57:27 -0400

    book.js

--- models/book.js
+++ models/book.js
@@ -3,7 +3,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema(
-  {}
+  {
+    title: {type: String, required: true},
+    author: {type: Schema.Types.ObjectID, ref: 'Author', required: true},
+    summary: {type: String, required: true},
+    isbn: {type: String, required: true},
+    genre: {type: Schema.Types.ObjectID, ref: 'Genre'}
+  }
 );
 
 //Export model
